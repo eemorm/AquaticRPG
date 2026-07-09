fa97f1970899b6e5264f1ac6b1256b2ac6648702
eemorm <eemorm@gmail.com>
2026-07-09 14:30:06 -0500

    setting up initial

--- src/Types.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Standard Libraries
-#include <cstdint>
-
-// Type Declarations
-using Entity = uint32_t;
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,10 @@
-#include "World.hpp"
-#include "Systems.hpp"
 #include "Camera.hpp"
 #include "TimeManager.hpp"
 #include "AssetManager.hpp"
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "ECS Test");
-
-    Camera cam({800, 600});
-    World world;
-
-    // Create player
-    Entity player = world.create();
-    world.transform[player] = {{100, 200}, {100, 0}};
-    world.playerControl[player] = {200.f};
-    world.sprite[player] = {sf::Sprite(Assets::getTexture("textures/attack.png"))};
-    world.health[player] = {100, 100};
-    world.players.insert(player);
+int main() 
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "AquaticRPG");
 
     while (window.isOpen()) {
         sf::Event e;
@@ -25,15 +13,7 @@ int main() {
 
         Time::update();
 
-        playerControlSystem(world);
-        movementSystem(world, Time::delta);
-
         window.clear();
-        //cam.follow(world.transform[player].pos);
-        //cam.apply(window);
-
-        renderSystem(world, window);
-
         window.setView(window.getDefaultView());
         window.display();
     }
